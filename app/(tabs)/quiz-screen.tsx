25eb63a66aa354d13f7b67879c0ba2034885fa29
Matheus Castro <matheuscastroks@gmail.com>
2025-04-25 20:19:57 -0300

    .

---
+++ app/(tabs)/quiz-screen.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, View } from "react-native";
+
+export default function QuizScreen() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
